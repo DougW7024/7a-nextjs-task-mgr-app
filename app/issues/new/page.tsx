d8826f377563bc932cf299e15bfb87fcfb5ca2d4
DAW <doug.wilkinson@gmail.com>
2024-07-14 12:59:23 -0400

    Add a spinner

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,13 @@
 "use client";
 
-import { Button, Callout, TextField, Theme, Text } from "@radix-ui/themes";
+import {
+  Button,
+  Callout,
+  TextField,
+  Theme,
+  Text,
+  Spinner,
+} from "@radix-ui/themes";
 // import { TextField } from "@radix-ui/react-textfield";
 import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from "react-hook-form";
@@ -26,6 +33,7 @@ const NewIssuePage = () => {
     resolver: zodResolver(createIssueSchema),
   });
   const [error, setError] = useState("");
+  const [isSubmitting, setSubmitting] = useState(false);
 
   return (
     <div className="max-w-xl">
@@ -38,9 +46,11 @@ const NewIssuePage = () => {
         className="space-y-3"
         onSubmit={handleSubmit(async (data) => {
           try {
+            setSubmitting(true);
             await axios.post("/api/issues", data);
             router.push("/issues");
           } catch (error) {
+            setSubmitting(false);
             setError("An unexpected error occurred.");
           }
         })}
@@ -60,7 +70,9 @@ const NewIssuePage = () => {
 
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
 
-        <Button>Submit New Issue</Button>
+        <Button disabled={isSubmitting}>
+          Submit New Issue {isSubmitting && <Spinner />}
+        </Button>
       </form>
     </div>
   );
